f8c9a927855d6dd90d93522662ea73656842d2ef
Emre Surmeli <s.emre.s.8@gmail.com>
2015-05-06 16:54:51 -0700

    environment setup

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+			simplemocha: {
+				dev: {
+					src: ['tests/*.js']
+				}
+			},
+
+			jshint: {
+				dev: {
+					src: ['Gruntfile.js', 'lib/*.js', 'tests/*.js']
+				},
+				options: {
+					jshintrc: true
+				}
+			},
+
+			watch: {
+				files: ['tests/*.js', 'lib/*.js'],
+				tasks: ['jshint:dev', 'simplemocha:dev']
+			}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-simple-mocha');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	//register default task
+	grunt.registerTask('default', ['test']);
+	grunt.registerTask('test', ['jshint:dev', 'simplemocha:dev']);
+};
